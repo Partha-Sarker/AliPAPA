54503f0a7b874447f11fcaedfab6662fe6eab3d1
Partha <parthasarker3@gmail.com>
2021-03-27 14:36:55 +0600

    10 products added

---
+++ resources/views/partials/_product.blade.php
@@ -0,0 +1,42 @@
+{{-- <div class="card">
+    <img class="card-img" src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/vans.png" alt="Vans">
+    <div class="card-img-overlay d-flex justify-content-end">
+    <a href="#" class="card-link text-danger like">
+        <i class="fas fa-heart"></i>
+    </a>
+    </div>
+    <div class="card-body">
+    <h4 class="card-title">Vans Sk8-Hi MTE Shoes</h4>
+    <h6 class="card-subtitle mb-2 text-muted">Style: VA33TXRJ5</h6>
+    <p class="card-text">
+        The Vans All-Weather MTE Collection features footwear and apparel designed to withstand the elements whilst still looking cool.</p>
+    <div class="options d-flex flex-fill">
+        <select class="custom-select mr-1">
+            <option selected>Color</option>
+            <option value="1">Green</option>
+            <option value="2">Blue</option>
+            <option value="3">Red</option>
+        </select>
+        <select class="custom-select ml-1">
+            <option selected>Size</option>
+            <option value="1">41</option>
+            <option value="2">42</option>
+            <option value="3">43</option>
+        </select>
+    </div>
+    <div class="buy d-flex justify-content-between align-items-center">
+        <div class="price text-success"><h5 class="mt-4">$125</h5></div>
+        <a href="#" class="btn btn-danger mt-3"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
+    </div>
+    </div>
+</div> --}}
+<div class="card p-15" style="max-width: 90%">
+    <a href={{$product->link}}>
+        <img class="card-img-top" src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/vans.png" alt="Card image cap"></a>
+    <div class="card-body">
+      <h5 class="card-title">Product Name</h5>
+      <p class="card-text">Product details.</p>
+      <a href="#" class="btn btn-primary mx-10">Buy Now</a>
+      <span class="px-10">price</span>
+    </div>
+  </div>
